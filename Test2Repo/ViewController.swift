a37a4e1924fca5e468df749e77d71bf9e99478f6
LubaKaper <liubovkaper@pursuit.com>
2020-02-13 11:14:30 -0500

    Initial Commit

---
+++ Test2Repo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test2Repo
+//
+//  Created by Liubov Kaper  on 2/13/20.
+//  Copyright © 2020 Luba Kaper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Test2RepoTests/Test2RepoTests.swift
@@ -0,0 +1,34 @@
+//
+//  Test2RepoTests.swift
+//  Test2RepoTests
+//
+//  Created by Liubov Kaper  on 2/13/20.
+//  Copyright © 2020 Luba Kaper. All rights reserved.
+//
+
+import XCTest
+@testable import Test2Repo
+
+class Test2RepoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
